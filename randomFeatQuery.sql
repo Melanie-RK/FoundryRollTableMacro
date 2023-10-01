e89745eafce7c491c4ff79cf1e124bc6ad72c073
Melanie <caafpiraat@gmail.com>
2023-10-01 19:42:08 +0200

    Query to randomly select a feat without prerequisites

---
+++ randomFeatQuery.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1) [name], [prerequisites],[prerequisite_feats],[prerequisite_skills] 
+	FROM [PFDB].[dbo].[Feats]
+	WHERE (prerequisite_feats IS NULL AND prerequisite_skills IS NULL AND prerequisites IS NULL)
+  ORDER BY NEWID();
