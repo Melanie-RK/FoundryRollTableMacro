9b22e08650e076cc6c9306180e5e680b8e79ca57
Bram Teurlings <bram.teurlings@live.nl>
2023-10-27 20:08:00 +0200

    Updated DB name in  queries

--- Queries/randomClassFeatQuery.sql
+++ Queries/randomClassFeatQuery.sql
@@ -1,4 +1,4 @@
   SELECT TOP (1) [class], [class_feature] AS feature_name, [description], [archetype], [sub_feature_of]
-  FROM [PFDB].[dbo].[ClassFeaturesSource]
+  FROM ClassFeaturesSource
   ORDER BY NEWID();
 
--- Queries/randomFeatQuery.sql
+++ Queries/randomFeatQuery.sql
@@ -1,4 +1,3 @@
 SELECT TOP (1) [name], [prerequisites],[prerequisite_feats],[prerequisite_skills] 
-	FROM [PFDB].[dbo].[Feats]
-	WHERE (prerequisite_feats IS NULL AND prerequisite_skills IS NULL AND prerequisites IS NULL)
+	FROM Feats
   ORDER BY NEWID();
