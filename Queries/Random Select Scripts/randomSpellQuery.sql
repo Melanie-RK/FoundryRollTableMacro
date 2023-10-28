0b4e2352d26c2608e7d3d4d5bebbf1b477e620f6
Melanie-RK <caafpiraat@gmail.com>
2023-10-28 18:22:13 +0200

    formatting fix

--- Queries/Random Select Scripts/randomSpellQuery.sql
+++ Queries/Random Select Scripts/randomSpellQuery.sql
@@ -1,8 +1,8 @@
 DECLARE @DesiredSpellLevel INT = 5;
 
 SELECT TOP (1) [name], [sorcerer], [wizard], [cleric], [druid], [ranger], [bard], [alchemist], [summoner], [witch], [inquisitor], [oracle], [antipaladin], [magus], [adept], [bloodrager], [shaman], [medium], [occultist], [mesmerist], [skald], [investigator], [hunter]
-FROM [PFDB].[dbo].[Spells]
-INNER JOIN [PFDB].[dbo].[Spells_Spell_Level] ON [PFDB].[dbo].[Spells].[Id] = [PFDB].[dbo].[Spells_Spell_Level].[Id]
+FROM [PFDB_NEW].[dbo].[Spells]
+INNER JOIN [PFDB_NEW].[dbo].[Spells_Spell_Level] ON [PFDB_NEW].[dbo].[Spells].[Id] = [PFDB_NEW].[dbo].[Spells_Spell_Level].[Id]
 WHERE @DesiredSpellLevel = (
     SELECT MIN(MIN_VALUE)
     FROM (VALUES
